65c0d9354c72bb4c5a40ac572d1bf6f7b1c41d9d
Vasilis K <kolpaxidis86@gmail.com>
2022-02-15 17:17:48 +0200

    New update
    • Added new feature modal window that shows card content
    • Added new css and  modal animation
    • html file added minore changes

--- js/script.js
+++ js/script.js
@@ -8,7 +8,13 @@ document.addEventListener('DOMContentLoaded', () => {
 		inpSurname = document.querySelector('.input-surname'),
 		inpEmail = document.querySelector('.input-email'),
 		form = document.querySelector('.profile'),
-		errorOut = document.querySelector('.error-output');
+		errorOut = document.querySelector('.error-output'),
+		modalProfile = document.querySelector('.modal__profile'),
+		result = document.querySelector('.result');
+
+	let userName,
+		userSurname,
+		userEmail;
 
 	// btn.addEventListener('click', () => {
 	// 	const div = document.createElement('div');
@@ -31,9 +37,9 @@ document.addEventListener('DOMContentLoaded', () => {
 	// });
 
 	btn.addEventListener('click', () => {
-		let userName = inpName.value,
-			userSurname = inpSurname.value,
-			userEmail = inpEmail.value;
+		userName = inpName.value;
+		userSurname = inpSurname.value;
+		userEmail = inpEmail.value;
 
 		if (userName, userSurname, userEmail) {
 			if (userName.length > 20) {
@@ -45,49 +51,64 @@ document.addEventListener('DOMContentLoaded', () => {
 			if (userEmail.length > 20) {
 				userEmail = `${userEmail.substring(0, 21)}...`;
 			}
+
 			form.innerHTML += `
 			<div class="card-body">
 				<div class="card-body-text">
-					<span class="card-body-span">Name: ${userName}</span>
-					<span class="card-body-span">Surname: ${userSurname}</span>
-					<span class="card-body-span">Email: ${userEmail}</span>
+					<span class="card-body-span card__name">Name: ${userName}</span>
+					<span class="card-body-span card__surname">Surname: ${userSurname}</span>
+					<span class="card-body-span card__email">Email: ${userEmail}</span>
 				</div>
-				<i class="trash fas fa-trash fa-2x"></i>
-				<i class="profile fas fa-id-card fa-2x"></i>
+				<i class="trash fas fa-trash fa-2x">trash</i>
+				<i class="profile-info fas fa-id-card fa-2x">profile</i>
 			</div>`;
 
 		} else {
-			errorOut.innerHTML = 'Error. Please fill all fields.';
+			errorOut.innerHTML = 'Error. Please fill all the fields.';
 			setTimeout(() => {
 				errorOut.innerHTML = '';
 			}, 3500);
 		}
-
-		// document.querySelectorAll('.trash').forEach((btn) => {   <<<<<<<<<<<<<<< OLD METHOD
-		// 	btn.addEventListener('click', () => {
-		// 		btn.parentElement.remove();
-		// 	});
-		// });
-
 	});
-
-	form.addEventListener('click', (e) => {
-		if (e.target && e.target.matches('.profile .card-body .trash')) {
-			e.target.parentElement.remove();
-		}
-
-	});
-
 
 	btnDelete.addEventListener('click', () => {
 		form.removeChild(form.lastChild);
 	});
 
 	btnDeleteAll.addEventListener('click', () => {
+		// Почему код ниже работает? Ведь удаляется первый ребёнок.
 		// while (form.firstChild) {
 		// 	form.removeChild(form.firstChild);
 		// }
 		form.innerHTML = '';
+	});
+
+
+	// ======================================= RESULT AREA ===================================================
+	// ------------- Delete Element \\\\\\\\ Open modal --------------
+	form.addEventListener('click', (e) => {
+		if (e.target && e.target.matches('.profile .card-body .trash')) {
+			e.target.parentElement.remove();
+		}
+		if (e.target && e.target.matches('.profile .card-body .profile-info')) {
+			let profText = e.target.parentElement.innerHTML;
+			profText = profText.split('<i')[0];
+
+			modalProfile.classList.add('active__modal');
+			modalProfile.classList.add('transform');
+			modalProfile.classList.remove('close__modal');
+
+			modalProfile.innerHTML = `<div class="modal__close">&times;</div> ${profText}`;
+		}
+	});
+
+	// modal close -------
+	result.addEventListener('click', (e) => {
+		if (e.target && e.target.matches('.result .modal__profile .modal__close')) {
+			modalProfile.classList.remove('active__modal');
+			modalProfile.classList.remove('transform');
+			modalProfile.classList.add('close__modal');
+		}
 	});
 
 });
